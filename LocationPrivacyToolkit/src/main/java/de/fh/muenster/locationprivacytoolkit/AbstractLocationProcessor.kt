f6af1667bbddd307af17718f1f68851661a56e71
Felix Erdmann <hello@felixerdmann.com>
2023-01-16 11:37:56 +0100

    rfc: add comments

--- LocationPrivacyToolkit/src/main/java/de/fh/muenster/locationprivacytoolkit/AbstractLocationProcessor.kt
+++ LocationPrivacyToolkit/src/main/java/de/fh/muenster/locationprivacytoolkit/AbstractLocationProcessor.kt
@@ -4,16 +4,34 @@ import android.content.Context
 import android.location.*
 
 /**
- * Abstract class which processes Location data
+ * Abstract class which processes a location
+ * It's meant to be implemented by a subclass
+ * which defines it's logic in the `process` function
+ *
+ * @param context The application context
  */
 abstract class AbstractLocationProcessor(context: Context) {
-    // must be implemented by subclass
+    // The configuration the subclass is processing. Must be implemented by subclass
     abstract var configKey: LocationPrivacyConfigKey
 
+    // The actual config from the LocationPrivacyConfig
     private var config: LocationPrivacyConfig = LocationPrivacyConfig(context)
 
+    /**
+     * Function can be consumed by the subclass to get
+     * the corresponding config value
+     *
+     * @return Value of the LocationPrivacyConfig
+     */
     fun getConfig(): Int? {
         return config.getPrivacyConfig(configKey)
     }
+
+    /**
+     * The function that processes a location based
+     *
+     * @param location A location object that will be manipulated
+     * @return A manipulated location
+     */
     abstract fun process(location: Location): Location
 }
--- LocationPrivacyToolkit/src/main/java/de/fh/muenster/locationprivacytoolkit/processors/AccuracyProcessor.kt
+++ LocationPrivacyToolkit/src/main/java/de/fh/muenster/locationprivacytoolkit/processors/AccuracyProcessor.kt
@@ -5,9 +5,18 @@ import android.location.*
 import de.fh.muenster.locationprivacytoolkit.AbstractLocationProcessor
 import de.fh.muenster.locationprivacytoolkit.LocationPrivacyConfigKey
 
+/**
+ * The AccuracyProcessor changes the accuracy of a location.
+ *
+ * @param context Application context
+ */
 class AccuracyProcessor(context: Context): AbstractLocationProcessor(context) {
     override var configKey = LocationPrivacyConfigKey.accuracy
 
+    /**
+     * The location will be moved to a random point around the actual
+     * point and the `accuracy` metadata will be changed as well
+     */
     override fun process(location: Location): Location {
         // get config or return location if config is null
         val config = this.getConfig() ?: return location
