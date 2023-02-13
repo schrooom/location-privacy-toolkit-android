91d16a5fed9215cbbe4333c728db6ba06a6df4e3
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2023-02-13 12:15:37 +0100

    update libs and add simple interval logic

--- LocationPrivacyToolkit/src/main/java/de/fh/muenster/locationprivacytoolkit/processors/IntervalProcessor.kt
+++ LocationPrivacyToolkit/src/main/java/de/fh/muenster/locationprivacytoolkit/processors/IntervalProcessor.kt
@@ -3,14 +3,28 @@ package de.fh.muenster.locationprivacytoolkit.processors
 import android.content.Context
 import android.location.*
 import de.fh.muenster.locationprivacytoolkit.AbstractLocationProcessor
+import de.fh.muenster.locationprivacytoolkit.LocationPrivacyConfig
 import de.fh.muenster.locationprivacytoolkit.LocationPrivacyConfigKey
 
 class IntervalProcessor(context: Context): AbstractLocationProcessor(context) {
     override var configKey = LocationPrivacyConfigKey.interval
 
-    override fun manipulateLocation(location: Location, config: Int): Location {
-        // TODO: implement interval logic
+    private val locationPrivacyConfig = LocationPrivacyConfig(context)
+    private var lastLocation: Location?
+        get() = locationPrivacyConfig.getLastLocation()
+        set(value) = locationPrivacyConfig.setLastLocation(value)
+    private val currentTime: Long
+        get() = System.currentTimeMillis()
 
+    override fun manipulateLocation(location: Location, config: Int): Location {
+        val cachedLocation = lastLocation
+        if (config > 0 && cachedLocation != null) {
+            val timeDiffToLastLocation = currentTime - cachedLocation.time
+            if (timeDiffToLastLocation < config) {
+                return cachedLocation
+            }
+        }
+        lastLocation = location
         return location
     }
 }
