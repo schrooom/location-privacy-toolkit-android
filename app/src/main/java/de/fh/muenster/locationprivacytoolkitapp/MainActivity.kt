e771ca14ffbc17c123b647e2c25993e975145242
gerrit:jdreyer <j.dreyer@fh-muenster.de>
2023-03-20 13:43:16 +0100

    improve debug-ui

--- app/src/main/java/de/fh/muenster/locationprivacytoolkitapp/MainActivity.kt
+++ app/src/main/java/de/fh/muenster/locationprivacytoolkitapp/MainActivity.kt
@@ -9,17 +9,17 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.mapbox.geojson.MultiPoint
 import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.style.layers.CircleLayer
-import com.mapbox.mapboxsdk.style.layers.FillLayer
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.mapboxsdk.style.layers.*
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import com.mapbox.turf.TurfConstants.UNIT_METERS
 import com.mapbox.turf.TurfTransformation
@@ -36,6 +36,8 @@ class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var locationToolkit: LocationPrivacyToolkit
     private val dateFormat = SimpleDateFormat("MM/dd HH:mm:ss")
+
+    private val lastLocations: MutableList<Location> = mutableListOf()
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,9 +56,19 @@ class MainActivity : AppCompatActivity(), LocationListener {
                 return@setOnClickListener
             }
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                locationToolkit.requestLocationUpdates(LocationManager.FUSED_PROVIDER, 1000, 10f, this)
+                locationToolkit.requestLocationUpdates(
+                    LocationManager.FUSED_PROVIDER,
+                    1000,
+                    10f,
+                    this
+                )
             } else {
-                locationToolkit.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10f, this)
+                locationToolkit.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    1000,
+                    10f,
+                    this
+                )
             }
             binding.startTrackingButton.isEnabled = false
             binding.stopTrackingButton.isEnabled = true
@@ -141,48 +153,67 @@ class MainActivity : AppCompatActivity(), LocationListener {
         binding.mapView.getMapAsync { map ->
             // Set the map view to new location
             centerMapTo(map, l.latitude, l.longitude)
+            lastLocations.add(l)
             addLocationToMap(map, l)
         }
+        addLocationToDebugText(l)
     }
 
     private fun addLocationToMap(map: MapboxMap, l: Location) {
         map.getStyle { style ->
-            val newLocationJson = Point.fromLngLat(l.longitude, l.latitude)
+            val newLocationPoints =
+                lastLocations.map { loc -> Point.fromLngLat(loc.longitude, loc.latitude) }
+            val newLocationMultiPoint = MultiPoint.fromLngLats(newLocationPoints)
             val oldLocationSource = style.getSource(POSITION_SOURCE_ID) as? GeoJsonSource
             if (oldLocationSource != null) {
-                oldLocationSource.setGeoJson(newLocationJson)
+                oldLocationSource.setGeoJson(newLocationMultiPoint)
             } else {
                 style.addSource(GeoJsonSource(POSITION_SOURCE_ID).apply {
-                    setGeoJson(newLocationJson)
+                    setGeoJson(newLocationMultiPoint)
                 })
             }
-            val haloData = TurfTransformation.circle(Point.fromLngLat(l.longitude, l.latitude), l.accuracy.toDouble(), UNIT_METERS)
+            val haloPolygon = TurfTransformation.circle(
+                Point.fromLngLat(l.longitude, l.latitude),
+                l.accuracy.toDouble(),
+                UNIT_METERS
+            )
+            //val haloMultiPolygon = MultiPolygon.fromPolygons(haloPolygons)
             val oldHaloSource = style.getSource(HALO_SOURCE_ID) as? GeoJsonSource
             if (oldHaloSource != null) {
-                oldHaloSource.setGeoJson(haloData)
+                oldHaloSource.setGeoJson(haloPolygon)
             } else {
                 style.addSource(GeoJsonSource(HALO_SOURCE_ID).apply {
-                    setGeoJson(haloData)
+                    setGeoJson(haloPolygon)
                 })
             }
             if (style.getLayer(POSITION_LAYER_ID) == null) {
-                val positionLayer = CircleLayer(POSITION_LAYER_ID, POSITION_SOURCE_ID).withProperties(
-                    PropertyFactory.circleRadius(5f),
-                    PropertyFactory.circleColor(Color.RED),
-                    PropertyFactory.circleOpacity(0.75f)
-                )
+                val positionLayer =
+                    CircleLayer(POSITION_LAYER_ID, POSITION_SOURCE_ID).withProperties(
+                        PropertyFactory.circleRadius(6f),
+                        PropertyFactory.circleColor(Color.RED),
+                        PropertyFactory.circleOpacity(0.75f)
+                    )
                 style.addLayer(positionLayer)
                 val haloLayer = FillLayer(HALO_LAYER_ID, HALO_SOURCE_ID).withProperties(
                     PropertyFactory.fillColor(Color.RED),
-                    PropertyFactory.fillOpacity(0.1f)
+                    PropertyFactory.fillOpacity(0.1f),
                 )
-                style.addLayer(haloLayer)
+                style.addLayerBelow(haloLayer, POSITION_LAYER_ID)
+                val lineLayer = LineLayer(LINE_LAYER_ID, POSITION_SOURCE_ID).withProperties(
+                    PropertyFactory.lineColor(Color.BLUE),
+                    PropertyFactory.lineOpacity(0.5f),
+                    PropertyFactory.lineWidth(4f),
+                    PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
+                    PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND)
+                )
+                style.addLayerBelow(lineLayer, HALO_LAYER_ID)
             }
         }
     }
 
-    private fun centerMapTo(map: MapboxMap, lat: Double, lon: Double, zoom: Double = POSITION_ZOOM) {
-        val camera = CameraUpdateFactory.newLatLngZoom(LatLng(lat, lon), zoom)
+    private fun centerMapTo(map: MapboxMap, lat: Double, lon: Double, zoom: Double? = null) {
+        val camera =
+            CameraUpdateFactory.newLatLngZoom(LatLng(lat, lon), zoom ?: map.cameraPosition.zoom)
         map.easeCamera(camera)
     }
 
@@ -194,13 +225,12 @@ class MainActivity : AppCompatActivity(), LocationListener {
                 style.removeLayer(LINE_LAYER_ID)
                 style.removeSource(POSITION_SOURCE_ID)
                 style.removeSource(HALO_SOURCE_ID)
-                style.removeSource(LINE_SOURCE_ID)
             }
         }
+        lastLocations.clear()
     }
 
-    // LocationListener
-    override fun onLocationChanged(l: Location) {
+    private fun addLocationToDebugText(l: Location) {
         val date = dateFormat.format(Date(l.time))
         val lat = BigDecimal(l.latitude).setScale(5, RoundingMode.HALF_EVEN)
         val lon = BigDecimal(l.longitude).setScale(5, RoundingMode.HALF_EVEN)
@@ -209,6 +239,15 @@ class MainActivity : AppCompatActivity(), LocationListener {
         val addLineString = if (binding.locationTextView.text.isBlank()) "" else "\n"
         val locationDebugText = "${binding.locationTextView.text}${addLineString}${locationString}"
         binding.locationTextView.text = locationDebugText
+        binding.locationScrollTextView.postDelayed({
+            binding.locationScrollTextView.fullScroll(
+                ScrollView.FOCUS_DOWN
+            )
+        }, 0)
+    }
+
+    // LocationListener
+    override fun onLocationChanged(l: Location) {
         processLocation(l)
     }
 
@@ -218,14 +257,12 @@ class MainActivity : AppCompatActivity(), LocationListener {
         private const val POSITION_LAYER_ID = "location_layer_id"
         private const val HALO_SOURCE_ID = "halo_source_id"
         private const val HALO_LAYER_ID = "halo_layer_id"
-        private const val LINE_SOURCE_ID = "line_source_id"
         private const val LINE_LAYER_ID = "line_layer_id"
 
-        private const val POSITION_ZOOM = 15.0
         // roughly Münster Westf.
         private const val INITIAL_LATITUDE = 51.961563
         private const val INITIAL_LONGITUDE = 7.628202
-        private const val INITIAL_ZOOM = 10.0
+        private const val INITIAL_ZOOM = 13.0
 
         // replace with proper style, if available
         private const val TILE_SERVER = "https://demotiles.maplibre.org/style.json"
